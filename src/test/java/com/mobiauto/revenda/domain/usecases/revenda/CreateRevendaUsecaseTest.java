bc90b928c60defe03470fcd40f1162af0c80ea15
Mateus Florencio <mateusflorecio@outlook.com.br>
2024-03-25 16:18:55 -0300

    refactor: alterando nome da classe de teste e adicionando id faltantes do modelo de revenda

--- src/test/java/com/mobiauto/revenda/domain/usecases/revenda/CreateRevendaUsecaseTest.java
+++ src/test/java/com/mobiauto/revenda/domain/usecases/revenda/CreateRevendaUsecaseTest.java
@@ -21,7 +21,7 @@ import com.mobiauto.revenda.domain.exceptions.RegistredException;
 import com.mobiauto.revenda.domain.models.revenda.RevendaModel;
 import com.mobiauto.revenda.domain.usecases.CreateRevendaUseCase;
 
-class CreateRevendaTest {
+class CreateRevendaUsecaseTest {
 
   @Mock
   private CreateRevendaRepository createRevendaRepository;
@@ -40,7 +40,7 @@ class CreateRevendaTest {
   @Test
   @DisplayName("Deve lançar uma exceção caso o CNPJ já esteja cadastrado")
   void deveLancarUmErroCasoARevendaJaEstejaCadastrada() {
-    RevendaModel revenda = new RevendaModel("123456789", "any_nome");
+    RevendaModel revenda = new RevendaModel("123456789", "any_nome", 1);
     Optional<RevendaModel> optionalRevenda = Optional.of(revenda);
     when(findByCnpjRepository.findByCnpj("123456789")).thenReturn(optionalRevenda);
 
@@ -52,7 +52,7 @@ class CreateRevendaTest {
   @Test
   @DisplayName("Deve criar uma revenda")
   void deveCriarUmaRevenda() throws RegistredException {
-    RevendaModel revenda = new RevendaModel("123456789", "any_nome");
+    RevendaModel revenda = new RevendaModel("123456789", "any_nome", 1);
     Optional<RevendaModel> optionalRevenda = Optional.empty();
     when(findByCnpjRepository.findByCnpj("123456789")).thenReturn(optionalRevenda);
     when(createRevendaRepository.create(new CreateRevendaDto("any_nome", "123456789"))).thenReturn(revenda);
